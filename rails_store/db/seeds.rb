6de333ba877b611fe1ca4c0f67a39cd6b1736ddf
Anna Khoma <annkhoma@gmail.com>
2015-06-02 21:38:56 +0300

    Rails generate scaffold

--- rails_store/db/seeds.rb
+++ rails_store/db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name: 'Apple', price: 500)
+Product.create(name: 'Asus', price: 350)
+Product.create(name: 'Acer', price: 400)
+Product.create(name: 'Vaio', price: 450) 
