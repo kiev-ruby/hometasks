f18e7d1945ed87290deff81d0bdea8b78c5069f2
Gena J <Jormungand718@gmail.com>
2015-06-25 12:31:47 +0800

    homework is done

---
+++ gena.razmakhnin/item.rb
@@ -0,0 +1,16 @@
+class Item
+
+	attr_accessor :name, :price, :quantity, :category
+
+	def initialize name, price, quantity = 1, category
+		if name.size > 5
+			@name = name
+			@price = price
+			@quantity = quantity
+			@category = category
+		else
+			raise'name of item is less then 5 letters'
+		end
+	end
+
+end
