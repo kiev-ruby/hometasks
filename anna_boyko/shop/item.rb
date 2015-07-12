6d1b796471aa6dfa044465d7eee2a6cff1e6edac
Boyko-Anna <boyko.anna.108@gmail.com>
2015-07-12 21:01:07 +0300

    Added to_s, fail in item.rb; made some changes in show_items, add_items, total_cost and other small changes in shop.rb

--- anna_boyko/shop/item.rb
+++ anna_boyko/shop/item.rb
@@ -2,9 +2,13 @@ class Item
   attr_accessor :name, :price, :category
 
   def initialize(name, price, category)
-    return puts 'The name of item is too short' if name.size < 5
+    fail('The name of item is too short') if name.size < 5
     @name = name
     @price = price
     @category = category
+  end
+
+  def to_s
+    "Item: #{@name}, price: #{@price}, category: #{@category}"
   end
 end
--- anna_boyko/shop/shop.rb
+++ anna_boyko/shop/shop.rb
@@ -3,40 +3,37 @@ class Shop
 
   def initialize(shop_name)
     return puts 'Shop already exists.' if @@shops.include?(shop_name)
+
     @@shops.push(shop_name)
     @shop_name = shop_name
-    @items ||= []
+    @items = []
   end
 
   def add_item(item, quantity = 1)
-    modified = false
-    @items.each do |each_item|
-      if each_item.include?(item)
-        each_item[1] += quantity
-        modified = true
-      end
+    index = @items.index { |shop_item| shop_item[0].name == item.name }
+    if index
+      @items[index][1] += quantity
+    else
+      @items.push([item, quantity])
     end
-    @items.push([item, quantity]) unless modified
     puts "#{quantity} item(s) \"#{item.name}\" have been successfully added"
   end
 
   def show_items(by = nil)
-    return puts "\"#{@shop_name}\" is empty." if @items.empty?
-    sort_items(by) if by
-    @items.each do |item|
-      puts "Item: #{item[0].name}, price: #{item[0].price}, " \
-        "quantity: #{item[1]}, category: #{item[0].category}"
+    sorted = by ? send(:sort_items, by) : @items
+    sorted.each do |item|
+      puts item[0].to_s << " - quantity: #{item[1]}"
     end
   end
 
   def sort_items(by)
     case by
     when 'name'
-      @items.sort! do |item1, item2|
+      return @items.sort do |item1, item2|
         item1[0].name.downcase <=> item2[0].name.downcase
       end
     when 'price'
-      @items.sort! do |item1, item2|
+      return @items.sort do |item1, item2|
         item1[0].price <=> item2[0].price
       end
     end
@@ -44,18 +41,20 @@ class Shop
 
   def show_category(cat)
     items = @items.select do |item|
-      item[0].category.downcase == cat.downcase
+      item[0].category == cat
     end
     items.each do |item|
-      puts "Item: #{item[0].name}, price: #{item[0].price}, " \
-        "quantity: #{item[1]}, category: #{item[0].category}"
+      puts item[0].to_s << " - quantity: #{item[1]}"
     end
   end
 
   # This method deletes all items if the quantity is not specified
+
   def delete_item(name, quantity = 0)
     index = @items.index { |item| item[0].name == name }
+
     return puts "There's no item \"#{name}" unless index
+
     if @items[index][1] > quantity && quantity != 0
       @items[index][1] -= quantity
       puts "#{quantity} item(s) \"#{name}\" have been successfully removed"
@@ -66,13 +65,8 @@ class Shop
   end
 
   def total_cost
-    if @items.empty?
-      puts "\"#{@shop_name}\" is empty."
-    else
-      total = 0
-      @items.each { |each_item| total += each_item[0].price * each_item[1] }
-      puts "Total cost of all items in \"#{@shop_name}\" is: #{total}."
-    end
+    total = @items.inject(0) { |sum, item| sum + item[0].price * item[1] }
+    puts "Total cost of all items in \"#{@shop_name}\" is: #{total}."
   end
 
   def batch_insert(items)
@@ -83,10 +77,13 @@ class Shop
   end
 
   # This method creates random shop
+
   def self.create_shop
     random = Random.new
+
     shop_name = "Shop_#{random.rand(1..20)}"
     shop = Shop.new(shop_name)
+
     items = []
     random.rand(20).times do
       item_name = "Item_#{random.rand(1..100)}"
@@ -96,6 +93,7 @@ class Shop
       item = Item.new(item_name, item_price, item_cat)
       items.push([item, item_quantity])
     end
+
     shop.batch_insert(items)
     shop
   end
