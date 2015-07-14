fe065ab6cefa9340830c831631e55761ba745338
AndreyMatvienko <minary1233@gmail.com>
2015-07-14 23:29:25 +0300

    Update shop.rb

--- andrey.matvienko/shop/shop.rb
+++ andrey.matvienko/shop/shop.rb
@@ -1,17 +1,19 @@
+# class shop
 class Shop
   attr_accessor :name, :items
 
-  def initialize name
+  def initialize(name, items = [])
     @name = name
-    @items = []
+    @items = items
   end
 
-  def add_item item
+  def add_item(item)
     @items << item
   end
 
-  def remove_item name
-    @item.delete_if { |item| item.name == name }
+  def remove_item
+    remove_item = @items.find { |item| item.name == name }
+    @items.delete(remove_item)
   end
 
   def show_items
@@ -23,27 +25,24 @@ class Shop
     puts items.sort { |x, y| x.price <=> y.price }
   end
 
-  def show_by_category category_name
+  def show_by_category(category_name)
     @items.each do |items|
       puts items if items.category = category_name
     end
   end
 
-  def show_by_name name
+  def show_by_name
     puts items.sort { |x, y| x.name <=> y.name }
   end
 
   def cost_items
-    total = 0
-    items.each do |items|
-      total += items.price
-    end
-    total
+    @items.inject(0) { |total, item| total + item.price }
   end
 
   def info
-    puts "Shop name: #{ name }"
-    puts "Total items cost: #{ cost_items } $"
+    puts "Shop name: #{name}"
+    puts "Total items cost: #{cost_items} $"
     puts "\n"
   end
 end
+
