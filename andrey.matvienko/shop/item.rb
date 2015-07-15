18ec7fa4253885fa91f5bf7986c3220d18db0e99
AndreyMatvienko <minary1233@gmail.com>
2015-07-15 14:57:36 +0300

    Update item.rb

--- andrey.matvienko/shop/item.rb
+++ andrey.matvienko/shop/item.rb
@@ -1,18 +1,16 @@
-# class items
+# class item
 class Item
   attr_accessor :item, :price, :quantity, :category
 
-  def initialize(item, price, category, quantity = 1)
-    item.size < 5 ? (fail 'items must be <=5 letters size') : @item = item
+  def initialize(item, price, quantity = 1, category)
+    @item = item
+    puts 'items must be <=5 letters size!' if item.size < 5
     @price = price
     @quantity = quantity
     @category = category
   end
 
   def to_s
-    "Item: #{item},
-      category: #{category},
-      price: #{price}$,
-      quantity: #{quantity}"
+    "Item:#{item}, category:#{category}, price:#{price}$, quantity:#{quantity}"
   end
 end
