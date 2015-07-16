7df311c16237f03b4ac2e6ec3088f4d5af6b3ca2
tema <artemkuchma@gmail.com>
2015-07-16 14:39:35 -0700

    Creates a method "delete_by_name", added for the method "add_item" a condition of adding the name of at least 5 symbols. Remove unnecessary blank lines.

--- artem.kuchma/shop/shop.rb
+++ artem.kuchma/shop/shop.rb
@@ -11,31 +11,41 @@ class Shop
 	end
 
 # Function of generation test content. n - number of items
-	def content_creator(n)
-		
+	def content_creator(n)		
 		n.to_i.times do |item|
 			item += 1
 			item = Item.new("Comp_item_#{item}", "cat", rand(1000), rand(5) )
-			@items.push(item)
-			
+			@items.push(item)			
 		end
 	end
 
-
-	def add_item(name, category, price, quantity)
-		
+	def add_item(name, category, price, quantity)		
 		item = Item.new(name, category, price, quantity)
-
 		items.each{|item|  @x = 1 if item.name == name}
-
-		if @x == 1
+		if name.size <5
+			puts "The name should contain more than 5 symbols"
+		elsif @x == 1
 			puts "This item alreadi exists!"
-		else
-				
+		else				
 			@items << item
 			#@items.each {|item| puts "#{item.name}, #{price}, #{quantity}"}
 		end
 	end
+
+	def list_all_items
+		items.each do |item|
+			puts item.to_s
+		end
+	end
+
+	def delete_by_name(name)
+		if items.reject! {|item| item.name == name}
+			puts "Item #{name} deleted!"
+		else
+			puts "Item not found. "
+		end
+	end
+
 
 
 end
@@ -43,5 +53,8 @@ end
 shop = Shop.new
 	#puts shop.print_shop_name
 	shop.content_creator(5)
-	shop.add_item("test", "cat", 23, 5)
+	shop.add_item("Comp_item_10", "cat", 23, 5)
+	shop.list_all_items
+	#shop.delete_by_name("tes")
+	#shop.list_all_items
 
