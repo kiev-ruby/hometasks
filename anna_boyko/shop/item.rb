7ce035de15973c5a18b3e3ab344ea29df5b1bd40
Boyko-Anna <boyko.anna.108@gmail.com>
2015-07-16 16:21:35 +0300

    Modify architecture of application, add ban on adding same item name to shop

--- anna_boyko/shop/item.rb
+++ anna_boyko/shop/item.rb
@@ -1,14 +1,16 @@
 class Item
-  attr_accessor :name, :price, :category
+  attr_accessor :name, :price, :quantity, :category
 
-  def initialize(name, price, category)
+  def initialize(name, price, quantity, category)
     fail('The name of item is too short') if name.size < 5
     @name = name
     @price = price
+    @quantity = quantity
     @category = category
   end
 
   def to_s
-    "Item: #{@name}, price: #{@price}, category: #{@category}"
+    "Item: #{@name}, price: #{@price}, " \
+    "quantity: #{@quantity}, category: #{@category}"
   end
 end
--- anna_boyko/shop/shop.rb
+++ anna_boyko/shop/shop.rb
@@ -2,81 +2,69 @@ class Shop
   @@shops = []
 
   def initialize(shop_name)
-    return puts 'Shop already exists.' if @@shops.include?(shop_name)
+    fail('Shop already exists.') if @@shops.include?(shop_name)
 
     @@shops.push(shop_name)
     @shop_name = shop_name
     @items = []
   end
 
+  # This method do not allow to add items with the same name
+
   def add_item(item, quantity = 1)
-    index = @items.index { |shop_item| shop_item[0].name == item.name }
-    if index
-      @items[index][1] += quantity
+    return if include_name?(item.name) && !@items.include?(item)
+
+    if @items.include?(item)
+      add_quantity(item.name, quantity)
     else
-      @items.push([item, quantity])
+      @items << item
     end
-    puts "#{quantity} item(s) \"#{item.name}\" have been successfully added"
+  end
+
+  def include_name?(name)
+    @items.any? { |item| item.name == name }
+  end
+
+  def add_quantity(name, quantity)
+    index = @items.index { |item| item.name == name }
+    @items[index].quantity += quantity
   end
 
   def show_items(by = nil)
-    sorted = by ? send(:sort_items, by) : @items
-    sorted.each do |item|
-      puts item[0].to_s << " - quantity: #{item[1]}"
-    end
+    send(:sort_items, by) if by
+    @items.each { |item| puts item.to_s }
   end
 
   def sort_items(by)
-    case by
-    when 'name'
-      return @items.sort do |item1, item2|
-        item1[0].name.downcase <=> item2[0].name.downcase
-      end
-    when 'price'
-      return @items.sort do |item1, item2|
-        item1[0].price <=> item2[0].price
-      end
-    end
+    @items.sort! { |item1, item2| item1.send(by) <=> item2.send(by) }
   end
 
   def show_category(cat)
-    items = @items.select do |item|
-      item[0].category == cat
-    end
-    items.each do |item|
-      puts item[0].to_s << " - quantity: #{item[1]}"
-    end
+    @items.each { |item| puts item.to_s if item.category == cat }
   end
 
   # This method deletes all items if the quantity is not specified
 
   def delete_item(name, quantity = 0)
-    index = @items.index { |item| item[0].name == name }
+    index = @items.index { |item| item.name == name }
 
-    return puts "There's no item \"#{name}" unless index
+    return unless index
 
-    if @items[index][1] > quantity && quantity != 0
-      @items[index][1] -= quantity
-      puts "#{quantity} item(s) \"#{name}\" have been successfully removed"
-    elsif @items[index][1] <= quantity || quantity == 0
+    if @items[index].quantity > quantity && quantity != 0
+      @items[index].quantity -= quantity
+    elsif @items[index].quantity <= quantity || quantity == 0
       @items.delete_at(index)
-      puts "Item \"#{name}\" has been removed"
     end
   end
 
   def total_cost
-    total = @items.inject(0) { |sum, item| sum + item[0].price * item[1] }
+    total = @items.inject(0) { |sum, item| sum + item.price * item.quantity }
     puts "Total cost of all items in \"#{@shop_name}\" is: #{total}."
   end
 
   def batch_insert(items)
-    items.each do |item, quantity|
-      quantity ||= 1
-      add_item(item, quantity)
-    end
+    items.each { |item| add_item(item) }
   end
-
-  # This method creates random shop
 
   def self.create_shop
     random = Random.new
@@ -84,17 +72,26 @@ class Shop
     shop_name = "Shop_#{random.rand(1..20)}"
     shop = Shop.new(shop_name)
 
+    items = create_items
+    shop.batch_insert(items)
+
+    shop
+  end
+
+  def self.create_items
+    random = Random.new
+
     items = []
-    random.rand(20).times do
+
+    10.times do
       item_name = "Item_#{random.rand(1..100)}"
       item_cat = "Cat_#{random.rand(1..10)}"
       item_price = random.rand(500..10_000)
       item_quantity = random.rand(1..50)
-      item = Item.new(item_name, item_price, item_cat)
-      items.push([item, item_quantity])
+      item = Item.new(item_name, item_price, item_quantity, item_cat)
+      items << item
     end
 
-    shop.batch_insert(items)
-    shop
+    items
   end
 end
