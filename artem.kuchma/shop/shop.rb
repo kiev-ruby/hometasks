dd8f3f348a9368fefd366d95226737efc515c3f2
tema <artemkuchma@gmail.com>
2015-07-18 15:14:49 -0700

    Added method to add_categories, method list_all_categorys, and the method  sort_by_category. Also modified the method  add_item, adding only provided the existing categories. Also modified the method content_creator by adding randomly adding categories

--- artem.kuchma/shop/shop.rb
+++ artem.kuchma/shop/shop.rb
@@ -6,25 +6,61 @@ class Shop
 		@items = items
 	end
 
+	@@shop_category = ["cat-1", "cat-2", "cat-3"]
+
 	def print_shop_name
 		"The name of your store: #{@shop_name}"
 	end
 
+
+
+
+	def add_category(category)
+		
+		if @@shop_category.count {|shop_category| shop_category == category } == 1
+			puts "This category alreadi exist"
+		else
+			@@shop_category << category
+		end
+	end
+
+	def list_all_categorys
+		puts "Shop categorys:"
+		@@shop_category.each {|shop_category| puts "#{shop_category}  " }
+		
+	end
+
 # Function of generation test content. n - number of items
-	def content_creator(n)		
+	def content_creator(n)
+		@n = @@shop_category.size		
 		n.to_i.times do |item|
 			item += 1
-			item = Item.new("Comp_item_#{item}", "cat", rand(1000), rand(5) )
+			item = Item.new("Comp_item_#{item}", "#{@@shop_category[rand(0..@n-1)]}", rand(1000), rand(5) )
 			@items.push(item)			
 		end
 	end
 
+
+
+
+
+
+
+
+
+
+
+
+
 	def add_item(name, category, price, quantity)		
 		item = Item.new(name, category, price, quantity)
-		if name.size <5
+		if name.size < 5
 			puts "The name should contain more than 5 symbols"
 		elsif @items.count{|item| item.name == name} == 1
 			puts "This item alreadi exists!"
+		elsif @@shop_category.count {|shop_category| shop_category == category} == 0
+			puts "\'#{category}\' category does not exist"
+
 		else				
 			@items << item
 			#@items.each {|item| puts "#{item.name}, #{price}, #{quantity}"}
@@ -67,17 +103,29 @@ class Shop
 		end
 	end
 
+	def sort_by_category(category)
+		i = items.select {|item| item.category == category}
+		i.each {|item| puts item.to_s}
+		puts "In \'#{category}\' category items is absent" if i.empty? 
+	end
+
 end
 
 shop = Shop.new
 	#puts shop.print_shop_name
-	shop.content_creator(5)
-	shop.add_item("testt", "cat", 23, 4)
-	shop.list_all_items
+	
+	
+	
 	#shop.delete_by_name("tes")
-	#shop.list_all_items
+	#
 	#shop.total_cost
-	shop.remove_same_items("testt", 3)
-	shop.list_all_items
-
-
+	#shop.remove_same_items("testt", 3)
+	#shop.list_all_items
+	shop.add_category("test")
+	shop.list_all_categorys
+shop.content_creator(5)
+#shop.list_all_items
+shop.add_item("testt", "cat-1", 23, 4)
+shop.list_all_items
+puts " "
+shop.sort_by_category("test")
