4e02c2b6fb0b82b3b8da27cc2fbd2756c7eb4b34
Boyko-Anna <boyko.anna.108@gmail.com>
2015-07-19 01:49:19 +0300

    Modify methods

--- anna_boyko/shop/shop.rb
+++ anna_boyko/shop/shop.rb
@@ -1,65 +1,39 @@
 class Shop
-  @@shops = []
+  attr_accessor :name, :items
 
-  def initialize(shop_name)
-    fail('Shop already exists.') if @@shops.include?(shop_name)
-
-    @@shops.push(shop_name)
-    @shop_name = shop_name
+  def initialize(name)
+    @name = name
     @items = []
   end
 
-  # This method do not allow to add items with the same name
-
-  def add_item(item, quantity = 1)
-    return if include_name?(item.name) && !@items.include?(item)
-
-    if @items.include?(item)
-      add_quantity(item.name, quantity)
-    else
-      @items << item
-    end
+  def add_item(item)
+    @items << item
   end
 
-  def include_name?(name)
-    @items.any? { |item| item.name == name }
-  end
-
-  def add_quantity(name, quantity)
-    index = @items.index { |item| item.name == name }
-    @items[index].quantity += quantity
+  def add_quantity(name, quantity = 1)
+    @items.find { |item| item.name == name }.quantity += quantity
   end
 
   def show_items(by = nil)
-    send(:sort_items, by) if by
+    @items.sort! { |item1, item2| item1.send(by) <=> item2.send(by) } if by
     @items.each { |item| puts item.to_s }
-  end
-
-  def sort_items(by)
-    @items.sort! { |item1, item2| item1.send(by) <=> item2.send(by) }
   end
 
   def show_category(cat)
     @items.each { |item| puts item.to_s if item.category == cat }
   end
 
-  # This method deletes all items if the quantity is not specified
+  def remove_item(name)
+    delete( @items.find { |item| item.name == name } )
+  end
 
-  def delete_item(name, quantity = 0)
-    index = @items.index { |item| item.name == name }
-
-    return unless index
-
-    if @items[index].quantity > quantity && quantity != 0
-      @items[index].quantity -= quantity
-    elsif @items[index].quantity <= quantity || quantity == 0
-      @items.delete_at(index)
-    end
+  def delete_items(name, quantity = 0)
+    @items.find { |item| item.name == name }.quantity -= quantity
   end
 
   def total_cost
     total = @items.inject(0) { |sum, item| sum + item.price * item.quantity }
-    puts "Total cost of all items in \"#{@shop_name}\" is: #{total}."
+    puts "Total cost of all items in \"#{@name}\" is: #{total}."
   end
 
   def batch_insert(items)
