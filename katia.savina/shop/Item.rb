254446d86201d48ad7103065105b95b4dc74223e
Katia Savina <santaminus@gmail.com>
2015-07-28 21:09:28 +0300

    Length verification fixed

--- katia.savina/shop/Item.rb
+++ katia.savina/shop/Item.rb
@@ -1,14 +1,16 @@
 class Item
   attr_accessor :name, :price, :number, :category
 
-  def initialize(name, price, number=1, category="Misc")
-  	if name.size > 4
-  	  @name = name
-  	  @price = price
-  	  @number = number
-  	  @category = category
-  	else
-  	  puts "ERROR: Too short name. An item hasn't been created"
-  	end
+  def initialize(name, price, number = 1, category = 'Misc')
+    fail 'ERROR: Too short name.' if name.size < 5
+    @name = name
+    @price = price
+    @number = number
+    @category = category
+
+    rescue
+      puts "\nERROR: Too short name. Enter a valid name (min 5 characters): "
+      name = gets.chomp
+      retry
   end
 end
