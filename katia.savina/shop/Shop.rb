5260f9182fa64f340c631a44e1a08cbd98466fbd
Katia Savina <santaminus@gmail.com>
2015-07-28 21:12:12 +0300

    Code refactoring

--- katia.savina/shop/Shop.rb
+++ katia.savina/shop/Shop.rb
@@ -1,82 +1,72 @@
-$LOAD_PATH << '.'
-require 'Item.rb'
+require_relative 'item.rb'
 
-class Shop 
+class Shop
   attr_reader :total_cost, :name
 
   def initialize(name)
-      @name = name
-      @goods = []
-      @total_cost = 0
+    @name = name
+    @goods = []
+    @total_cost = 0
   end
 
   def add_item(item)
-  	unless item.name.nil?
-  	  index = @goods.index {|i| i.name == item.name}
-      unless index.nil? 
-        @goods[index].number += item.number
-      else
-        @goods.push(item)
-      end
-      @total_cost += item.price * item.number
+    index = @goods.index { |i| i.name == item.name }
+    if index.nil?
+      @goods.push(item)
+    else
+      @goods[index].number += item.number
     end
+    @total_cost += item.price * item.number
   end
 
-  def delete(name, number=nil)
-    to_delete = @goods.select {|item| item.name==name}
-    unless to_delete[0].nil? 
-      if number.nil?
-        puts "\n#{to_delete[0].name} deleted"
-        @goods.delete_if {|item| item.name==name}
-        @total_cost -= to_delete[0].price
-      else
-        puts "\n#{number} pieces of #{to_delete[0].name} deleted"
-        index = @goods.index {|i| i.name == name}
-        @goods[index].number -= number
-        @total_cost -= @goods[index].price
-      end
+  def delete(name, number = nil)
+    to_delete = @goods.find { |item| item.name == name }
+    if number.nil?
+      @goods.delete_if { |item| item.name == name }
+      @total_cost -= to_delete.price
+    else
+      index = @goods.index { |i| i.name == name }
+      @goods[index].number -= number
+      @total_cost -= @goods[index].price
     end
   end
 
   def display_goods
-  	puts "\nGoods list "
+    puts "\nGoods list "
     @goods.each do |i|
       puts "#{i.name} \$#{i.price} #{i.number} pieces #{i.category}"
     end
   end
 
-  def get_total
+  def display_total
     puts "\nTotal: #{@total_cost}"
   end
 
   def select_goods(category)
-    selection = @goods.select {|item| item.category == category}
+    selection = @goods.select { |item| item.category == category }
     selection.each do |item|
-      puts "#{item.name}  \$#{item.price}  #{item.number}"
+      puts "#{item. name}  \$#{item.price}  #{item.number}"
     end
-    return selection
   end
 
-  def add_many items
-  	puts items.size
-  	items.each do |item|
-     index = @goods.index {|i| i.name == item.name}
-     unless index.nil? 
-        @goods[index].number += item.number
-      else
+  def add_many(items)
+    items.each do |item|
+      index = @goods.index { |i| i.name == item.name }
+      if index.nil?
         @goods.push(item) if item.is_a? Item
         puts item.to_s
+      else
+        @goods[index].number += item.number
       end
       @total_cost += item.price * item.number
     end
-  	#@goods += items
   end
 
   def order_by_name(name)
-    selection = @goods.select {|item| item.name == name}
+    @goods.select { |item| item.name == name }
   end
 
   def order_by_price(price)
-    selection = @goods.select {|item| item.price == price}
+    @goods.select { |item| item.price == price }
   end
 end
