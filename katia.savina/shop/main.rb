a135f47ef0f8cf77055df964aa50e16c77ffbf40
SantaMinus <santaminus@gmail.com>
2015-08-03 15:05:28 +0300

    Fixed

--- katia.savina/shop/main.rb
+++ katia.savina/shop/main.rb
@@ -12,23 +12,23 @@ def randomize
     category = ''
     7.times { item_name << chars.split('').sample }
     7.times { category << chars.split('').sample }
-    rand_shop.add_item(Item.new(item_name, rand(100), rand(50), category))
+    rand_shop.add_items(Item.new(item_name, rand(100), rand(50), category))
   end
   rand_shop
 end
 
 random_shop = randomize
-random_shop.display_goods(1)
+random_shop.display_goods(:name, :desc)
 
 # test
 shop = Shop.new('my_shop')
 
 item0 = Item.new('mouse', 15, 3)
-shop.add_item(item0)
+shop.add_items(item0)
 item1 = Item.new('screen', 25, 2, 'Peripherals')
-shop.add_item(item1)
+shop.add_items(item1)
 item2 = Item.new('screen', 15, 3)
-shop.add_item(item2)
+shop.add_items(item2)
 
 shop.display_goods
 shop.display_total
@@ -38,19 +38,10 @@ item4 = Item.new('SDcard', 25, 6, 'Memory')
 item5 = Item.new('flash', 10, 10, 'Memory')
 item6 = Item.new('keyboard', 10, 10, 'Peripherals')
 
-shop.add_many [item3, item4, item5, item6]
-shop.delete('SDcard', 3)
+shop.add_items [item3, item4, item5, item6]
+shop.delete('SDcard')
 shop.set_number('keyboard', 29)
-shop.display_goods(2)
+shop.display_goods(:price)
 shop.display_total
 
-cart1 = shop.order_by_name('SDcard')
-cart2 = shop.order_by_price(25)
-
-cart1.each do |item|
-  puts "#{item.name}"
-end
-
-cart2.each do |item|
-  puts "#{item.name}"
-end
+shop.select_goods('Peripherals')
--- katia.savina/shop/shop.rb
+++ katia.savina/shop/shop.rb
@@ -6,64 +6,46 @@ class Shop
     @goods = []
   end
 
-  def add_item(item)
-    @goods << item
+  def add_items(items)
+    if items.is_a? Array
+      items.each do |item|
+        @goods << item
+      end
+    else
+      @goods << items
+    end
   end
 
-  def delete(name, number = nil)
-    if number.nil?
-      @goods.delete_if { |item| item.name == name }
-    else
-      index = @goods.index { |i| i.name == name }
-      @goods[index].number -= number
-    end
+  def delete(name)
+    @goods.delete_if { |item| item.name == name }
   end
 
   def set_number(name, n)
-    index = @goods.index { |i| i.name == name }
-    @goods[index].number = n
+    @goods.find { |item| item.name == name } .number = n
   end
 
-  def display_goods(param = 1)
-    case param
-    when 1 then @goods.sort_by! { |item| item.name }
-    when 2 then @goods.sort_by! { |item| item.price }
-    when 3 then @goods.sort_by! { |item| item.number }
-    when 4 then @goods.sort_by! { |item| item.category }
-    else puts 'Cannot be sorted'
-    end
+  def display_goods(sym = nil, type = :asc)
+    @goods.sort_by!(&sym) if sym == :name || sym == :price || sym == :number || sym == :category
+    @goods.reverse! if type == :desc
     puts "\nGoods list "
-    @goods.each do |i|
-      puts "#{i.name} \$#{i.price} #{i.number} pieces #{i.category}"
-    end
+    @goods.each(&:to_s)
   end
 
   def display_total
-    total = 0
-    @goods.each do |i|
-      total += i.price * i.number
-    end
+    total = @goods.inject(0) { |a, e| a + e.price * e.number }
     puts "\nTotal: #{total}"
   end
 
   def select_goods(category)
     selection = @goods.select { |item| item.category == category }
-    selection.each do |item|
-      puts "#{item. name}  \$#{item.price}  #{item.number}"
-    end
+    selection.each(&:to_s)
   end
 
-  def add_many(items)
-    items.each do |item|
-      @goods << item
-    end
+  def order_by_name
+    @goods.sort_by!(&:name)
   end
 
-  def order_by_name(name)
-    @goods.select { |item| item.name == name }
-  end
-
-  def order_by_price(price)
-    @goods.select { |item| item.price == price }
+  def order_by_price
+    @goods.sort_by!(&:price)
   end
 end
